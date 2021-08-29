1921382c42a53f970ae7493c5aa8b55e5262c7f1
djibba22 <johnwaynedinsmore@gmail.com>
2021-08-29 00:06:45 -0400

    initial folder setup

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">   
+          <div className="container">
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+
+
+import { QUERY_PROFILES } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_PROFILES);
+  const profiles = data?.profiles || [];
+
+  return (
+    <main>
+    </main>
+  );
+};
+
+export default Home;
